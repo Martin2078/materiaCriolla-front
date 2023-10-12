1cadf2e7c605f8cf740a6c50110bb0a803a5a646
Martin <martinleonelgalan@gmail.com>
2023-10-12 16:33:08 -0300

    app

--- src/components/ProductsLayout.jsx
+++ src/components/ProductsLayout.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Details from './Details'
 
 const ProductsLayout = (props) => {
 const {productos, categoryChecked, setProductos, productosFiltrados, searchValue} =props
-
+const [change,setChange]=useState(false)
 useEffect(()=>{
   },[])
   
@@ -26,12 +27,12 @@ useEffect(()=>{
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {producto.name} </h5>
           
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{producto.description} </p>
-          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <button onClick={()=>setChange(true)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Agregar al carrito
                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
               </svg>
-          </a>
+          </button>
       </div>
   </div>
   
@@ -46,6 +47,7 @@ useEffect(()=>{
 
   return (
     <div className='w-full'>
+      {change&&<Details/>}
     <div className= 'w-full flex p-2 '>
          
         
@@ -57,7 +59,6 @@ mapear()
    
 </div>
 </div>
-
   )
 }
 
