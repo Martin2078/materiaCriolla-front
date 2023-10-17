0f6ebfccae159661de9ee6f9a8fb06033a667fb9
pip3campos <felipeacampose@gmail.com>
2023-10-17 00:17:22 -0300

    filtro por categoria

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -8,10 +8,16 @@ const Products = () => {
     const categories = useSelector((store) => store.categories.categories)
     const [change, setChange] = useState(false)
     const [detail, setDetail] = useState(null)
-    const checked = document.querySelectorAll('input[type=checkbox]:checked')
-    const arrayChecked = Array.from( checked ).map( checkbox => checkbox.value)
-    console.log(products)
-    console.log(categories)
+    const [checked, setChecked] = useState([])
+    const arrayChecked = checked.map(checkbox => checkbox)
+    const handleCheckboxChange = (event) => {
+        const { value, checked } = event.target
+        if (checked) {
+            setChecked((prevChecked) => [...prevChecked, value ])
+        } else {
+            setChecked((prevChecked) => prevChecked.filter((item) => item !== value))
+        }
+    }
     function filterByCategory ( products, categories) {
         if (categories.length == 0 ) {
             return products
@@ -30,15 +36,15 @@ const Products = () => {
         <div className='h-full w-full flex items-center'>
             {change && <Details detail={detail} change={change} setChange={setChange} />}
             <div className='lg:w-3/6 min-[320px]:w-5/6 bg-white flex items-center  lg:rounded-lg lg:px-4 lg:py-1 gap-2 min-[320px]:rounded-full min-[320px]:py-2 min-[320px]:px-2'>
-                <button className='bg-white rounded'><img src="../../public/images/search-icon.png" alt="" /></button>
+                <button className='bg-white rounded'><img src="/images/search-icon.png" alt="" /></button>
                 <input id='search' type="search" className='w-full h-8 border-none text-center' placeholder='Find Your Product Here' />
             </div>
             <div className='flex flex-col items-center h-fit bg-white lg:-translate-y-10 min-[320px]:-translate-y-12 py-5 lg:rounded-xl lg:w-11/12 min-[320px]:w-full min-[320px]:rounded-t-[55px]'>
                 <div className='w-3/6 flex justify-evenly mb-10 mt-5 min-[320px]:gap-2 lg:gap-0'>
                     {(categories.length > 0) ? categories.map((category) => (
-                        <div>
-                            <input type="checkbox" id={category._id} value={category.name}/>
-                            <label for={category._id}>{category.name}</label>
+                        <div key={category._id}>
+                            <input type="checkbox" id={category._id} value={category.name} onChange={handleCheckboxChange}/>
+                            <label htmlFor={category._id}>{category.name}</label>
                         </div>
                     )
                     ) : <p>We're sorry, there's no match!</p>
@@ -54,7 +60,7 @@ const Products = () => {
                                 }} src={product.product_photo} alt="" />
                                 <h2 className='text-black font-bold lg:w-full min-[320px]:w-5/6'>{product.name}</h2>
                                 {(product.colors.length > 0) ? product.colors.map((color) => (
-                                    <div className='h-8 flex items-center justify-center py-1 px-2 w-fit border border-black border-dashed'>
+                                    <div key={color} className='h-8 flex items-center justify-center py-1 px-2 w-fit border border-black border-dashed'>
                                         <p>{color}</p>
                                     </div>
                                 )) : <div>
